f37194ec3ea7c25e4709288328f79fcf8f1a144e
Alec <Alec@Charitys-MacBook-Pro.local>
2018-03-22 16:17:06 +1100

    More juice

--- views/main.js
+++ views/main.js
@@ -15,47 +15,55 @@ function view (state, emit) {
 					<h2 class="tc">BUSINESS MODEL CANVAS</h2>
 				</div>
 				<div class="fl w-100 w-third-ns w-50-m pa2">
-					<div class="${((state.canvas.value_prop_values.length > 0) ? 'bg-washed-green' : 'bg-white')} pv4 link dim ba b--black-20 br2 pointer" onclick=${handleClick} id="value-proposition">
+					<div class="pv4 link dim ba b--black-20 br2 pointer pa4" onclick=${handleClick} id="value-proposition">
 						<h4 class="tc">VALUE PROPOSITION</h4>
-					</div>
+            ${addDefinitionList('value-proposition')}
+          </div>
 				</div>
 				<div class="fl w-100 w-third-ns w-50-m pa2">
-					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer" onclick=${handleClick} id="customer-segments">
+					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer pa4" onclick=${handleClick} id="customer-segments">
 						<h4 class="tc">CUSTOMER SEGMENTS</h4>
+            ${addDefinitionList('customer-segments')}
 					</div>
 				</div>
 				<div class="fl w-100 w-third-ns h-5-ns w-50-m h-4-m pa2">
-					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer" onclick=${handleClick} id="customer-relationships">
+					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer pa4" onclick=${handleClick} id="customer-relationships">
 						<h4 class="tc">CUSTOMER RELATIONSHIPS</h4>
+            ${addDefinitionList('customer-relationships')}
 					</div>
 				</div>
 				<div class="fl w-100 w-third-ns w-50-m pa2">
-					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer" onclick=${handleClick} id="revenue">
+					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer pa4" onclick=${handleClick} id="revenue">
 						<h4 class="tc">REVENUE</h4>
+            ${addDefinitionList('revenue')}
 					</div>
 				</div>
 				<div class="fl w-100 w-third-ns w-50-m pa2">
-					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer" onclick=${handleClick} id="channels">
+					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer pa4" onclick=${handleClick} id="channels">
 						<h4 class="tc">CHANNELS</h4>
+            ${addDefinitionList('channels')}
 					</div>
 				</div>
 				<div class="fl w-100 w-third-ns w-50-m pa2">
-					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer" onclick=${handleClick} id="costs">
+					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer pa4" onclick=${handleClick} id="costs">
 						<h4 class="tc">COSTS</h4>
+            ${addDefinitionList('costs')}
 					</div>
 				</div>
 				<div class="fl w-100 w-third-ns w-100-m pa2">
-					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer" onclick=${handleClick} id="key-activities">
+					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer pa4" onclick=${handleClick} id="key-activities">
 						<h4 class="tc">KEY ACTIVITIES</h4>
+            ${addDefinitionList('key-activities')}
 					</div>
 				</div>
 				<div class="fl w-100 w-third-ns w-100-m pa2">
-					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer" onclick=${handleClick} id="partners">
+					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer pa4" onclick=${handleClick} id="partners">
 						<h4 class="tc">PARTNERS</h4>
+            ${addDefinitionList('partners')}
 					</div>
 				</div>
 				<div class="fl w-100 w-third-ns w-100-m pa2">
-					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer" onclick=${handleClick} id="key-resources">
+					<div class="bg-white pv4 link dim ba b--black-20 br2 pointer pa4" onclick=${handleClick} id="key-resources">
 						<h4 class="tc">KEY RESOURCES</h4>
 					</div>
 				</div>
@@ -70,8 +78,25 @@ function view (state, emit) {
 		  </div>
 		</footer>
     </body>
-	
   `
+
+  function addDefinitionList(area) {
+
+    const content = state.canvas.content[area];
+
+    return html`
+    <dl>
+      ${content.selected.map(function(index) {
+        return html`
+        <div>
+          <dt><strong class="green">${content.prompts[index]}</strong></dt>
+          <dd>${content.answers[index]}</dd>
+        </div>
+        `
+      })}
+    </dl>
+    `
+  }
 
   function handleClick (event) {
 	const newTitle = event.target.children[0].innerHTML;
